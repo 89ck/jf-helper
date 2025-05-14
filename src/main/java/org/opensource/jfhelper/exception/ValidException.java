5c99ae7999c1f26540ae7e511ce627237a0c489d
kxl <939624959@qq.com>
2025-05-14 09:39:28 +0800

    优化抛出的ValidException信息

--- src/main/java/org/opensource/jfhelper/exception/ValidException.java
+++ src/main/java/org/opensource/jfhelper/exception/ValidException.java
@@ -2,16 +2,39 @@ package org.opensource.jfhelper.exception;
 
 /**
  * 验证抛出的错误，用于验证提示
+ *
  * @author seiya
  */
 @SuppressWarnings("serial")
-public class ValidException extends RuntimeException{
+public class ValidException extends RuntimeException {
+
+    private int code = 500;
 
     /**
      * 构造函数
+     *
      * @param message
      */
     public ValidException(String message) {
         super(message);
+    }
+
+    /**
+     * 构造函数
+     * @param message
+     * @param code
+     */
+    public ValidException(String message, int code) {
+        super(message);
+        this.code = code;
+    }
+
+    /**
+     * 获取错误码， 默认500
+     *
+     * @return
+     */
+    public int getCode() {
+        return code;
     }
 }
--- src/main/java/org/opensource/jfhelper/interceptor/ValidInterceptor.java
+++ src/main/java/org/opensource/jfhelper/interceptor/ValidInterceptor.java
@@ -9,21 +9,22 @@ import com.jfinal.kit.Ret;
 /**
  * 全局捕获ValidException，并抛出异常提示。 <br/>
  * 抛出的形式以Json返回。
- * 
- * @author seiya
  *
+ * @author seiya
  */
 public class ValidInterceptor implements Interceptor {
 
-	@Override
-	public void intercept(Invocation inv) {
+    @Override
+    public void intercept(Invocation inv) {
 
-		try {
-			inv.invoke();
-		} catch (ValidException e) {
-			inv.getController().renderJson(Ret.fail(e.getMessage()));
-		}
+        try {
+            inv.invoke();
+        } catch (ValidException e) {
+            Ret validRet = Ret.fail().set("message", e.getMessage()).set("code", e.getCode());
+            inv.setReturnValue(validRet);
+            inv.getController().renderJson(validRet);
+        }
 
-	}
+    }
 
 }
